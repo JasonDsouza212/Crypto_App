220e78d255b1f9de4dbfab3f90a7c4a988fcace7
Jason Dsouza <jasondsouza212@gmail.com>
2021-10-25 22:07:45 +0530

    footer and some ant design features are added today

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            Crypto details
+        </div>
+    )
+}
+
+export default CryptoDetails
--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-ReactDom.render(<App/> ,document.getElementById('root'));
+import {BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.css';
+
+ReactDom.render(
+    <Router>
+        <App/>
+    </Router>, document.getElementById('root')
+    );
+    
