660ec9dab92019f2f5fe1b16913c5d26354d25cd
Jason Dsouza <jasondsouza212@gmail.com>
2021-10-30 19:27:43 +0530

    API database is not connecting

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -16,7 +16,7 @@ const Homepage = () => {
        <>
        <Title level={2} className="heading">Global Crypto Stats</Title>
        <Row>
-           <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+           <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
            <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
            <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
            <Col span={12}><Statistic title="Total 24h volume" value="5" /></Col>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,10 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const cryptoApiHeaders={
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': 'a33ef7bf89mshd1cdc0e911818fap1f2d40jsna4d412ecbaf9'
-}
+
+      'x-rapidapi-key': 'a33ef7bf89mshd1cdc0e911818fap1f2d40jsna4d412ecbaf9',
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+
+    };
 const baseUrl='https://coinranking1.p.rapidapi.com/stats';
 const createRequest =(url)=>({url,headers:cryptoApiHeaders});
 
@@ -11,7 +13,7 @@ export const cryptoApi=createApi({
     baseQuery:fetchBaseQuery({baseUrl}),
     endpoints:(builder)=>({
         getCryptos:builder.query({
-            query:()=>createRequest('/coins')
+            query:()=>createRequest(`/coins/`)
         })
     })
 })
