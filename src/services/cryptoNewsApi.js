34234e0183c45212f0f3e6fcdf14cf2d43192ed9
Jason Dsouza <jasondsouza212@gmail.com>
2021-11-02 00:25:02 +0530

    News section and filter about news ,how more about news all are updated

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,20 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders={
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'a33ef7bf89mshd1cdc0e911818fap1f2d40jsna4d412ecbaf9'
+  }
+  const baseUrl= 'https://bing-news-search1.p.rapidapi.com';
+  const createRequest =(url)=>({url,headers:cryptoNewsHeaders});
+  
+  export const cryptoNewsApi=createApi({
+    reducerPath:'cryptoNewsApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+        getCryptoNews:builder.query({
+            query:({newsCategory,count})=>createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+})
+export const {useGetCryptoNewsQuery} =cryptoNewsApi;
