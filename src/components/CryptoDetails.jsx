19ffe7866bca5bb1e57cbecb5494580d163dd999
Jason Dsouza <jasondsouza212@gmail.com>
2021-11-04 14:10:25 +0530

    crypto specific details are being added [on going]

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router';
+import millify from 'millify';
+import { Col,Row,Typography,Select } from 'antd';
+import { MoneyCollectOutlined , Dollar } from '@ant-design/icons';
 
 const CryptoDetails = () => {
     return (
