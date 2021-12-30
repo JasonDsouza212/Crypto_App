171040b165c2729e81617069d9434dd5229f5187
Jason Dsouza <jasondsouza212@gmail.com>
2021-12-30 21:51:40 +0530

    title and Name of app is changed to coins mart

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = ()=> {
             <div className="logo-container">
                  <Avatar src={icon} size="large" />
                  <Typography.Title level={2} className="logo">
-                     <Link to="/">Cryptoverse</Link>
+                     <Link to="/">Coins Mart</Link>
                  </Typography.Title>
                  <Button className="menu-control-container" onClick={()=>setActiveMenu(!activeMenu)}>
                      <MenuOutlined/>
